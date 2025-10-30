2b130c43b854efad018a578938785ba10b592515
Blessing Tembo <temboblessing05@gmail.com>
2025-10-31 01:02:26 +0200

    Change the icons & Add dark mode support with theme toggle

---
+++ scripts/themeSwitching.js
@@ -0,0 +1,22 @@
+const button = document.getElementById('theme-toggle');
+const body = document.body;
+const storedTheme = localStorage.getItem('theme');
+
+if (storedTheme === 'dark' || (
+    storedTheme === null && window.matchMedia('(prefers-color-scheme:dark)'))) {
+  body.classList.add('dark-mode');
+  button.innerHTML = '<img src="images/icons/sun-solid-full.svg" alt="moon/sun icon">';
+}
+
+button.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  const isDark = body.classList.contains('dark-mode');
+  const theme = isDark ? "dark" : "light";
+
+  button.innerHTML = isDark ? `
+    <img src="images/icons/sun-solid-full.svg" alt="moon/sun icon">` 
+    : `  
+    <img src="images/icons/moon-solid-full.svg" alt="moon/sun icon">` ;
+
+  localStorage.setItem('theme', theme);
+})
