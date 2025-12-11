f2ebcfcb6d8ef2b068a6a65287d887f0f17a4964
Blessing Tembo <temboblessing05@gmail.com>
2025-12-11 14:09:54 +0200

    adjust renderTechboxes.js file path

--- scripts/renderTechboxes.js
+++ scripts/renderTechboxes.js
@@ -1,12 +1,12 @@
 const technologies = [
-  { name: 'HTML5', logo: 'images/logos/html.png' },
-  { name: 'CSS3', logo: 'images/logos/css-3.png' },
-  { name: 'Javascript', logo: 'images/logos/js.png' },
-  { name: 'Responsive', logo: 'images/logos/devices.png' },
-  { name: 'React', logo: 'images/logos/React.png' },
-  { name: 'Node.js', logo: 'images/logos/Node.js.png' },
-  { name: 'PHP', logo: 'images/logos/php.png' },
-  { name: 'Python', logo: 'images/logos/python.png' }
+  { name: 'HTML5', logo: './images/logos/html.png' },
+  { name: 'CSS3', logo: './images/logos/css-3.png' },
+  { name: 'Javascript', logo: './images/logos/js.png' },
+  { name: 'Responsive', logo: './images/logos/devices.png' },
+  { name: 'React', logo: './images/logos/React.png' },
+  { name: 'Node.js', logo: './images/logos/Node.js.png' },
+  { name: 'PHP', logo: './images/logos/php.png' },
+  { name: 'Python', logo: './images/logos/python.png' }
 ];
 
 export function renderTechBoxes() {
