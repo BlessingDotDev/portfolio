1d7ac9d45041339ce8f77a8e8a8587608c4ce122
Blessing Tembo <temboblessing05@gmail.com>
2026-02-05 21:39:23 +0200

    Change new project link

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,8 +6,8 @@ const projects = [
     name: 'react-ecommerce-project',
     image: './images/projects/mpahla-pic.png',
     description: 'E-commerce Platform (React, Vite, Node.js, MongoDB) Developed a full-stack, responsive e-commerce application with a custom RESTful API backend. Implemented secure user authentication, dynamic product catalog, shopping cart, and order tracking functionality. Optimized for performance and scalability, demonstrating proficiency in modern frontend frameworks, backend API design, and database integration.',
-    gitHubLink: 'https://github.com/BlessingDotDev/react-ecommerce-mpahla.git',
-    siteLink: 'https://github.com/BlessingDotDev/mpahla-ecommerce-backend.git',
+    gitHubLink: 'https://github.com/BlessingDotDev/mpahla-ecommerce-frontend.git',
+    siteLink: 'https://mpahla-frontend.vercel.app/',
     deployed: true,
     tech: [
       {name: 'react', logo: './images/logos/React.png' },
